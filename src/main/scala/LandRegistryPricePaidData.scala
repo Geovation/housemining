6c3cfb4c5a9bef5a3aaa6f5ab063ce4b8bbd2d62
Sebastian Ovide <Sebastian.Ovide@gmail.com>
2016-05-15 19:00:39 +0100

    constructor optional parameter

--- src/main/scala/LandRegistryPricePaidData.scala
+++ src/main/scala/LandRegistryPricePaidData.scala
@@ -1,11 +1,11 @@
 import scala.collection.immutable.TreeMap
 
-class LandRegistryPricePaidData(private val ppds: List[LandRegistryPricePaidDataItem] ) {
+class LandRegistryPricePaidData(private val ppds: List[LandRegistryPricePaidDataItem] = List() ) {
 
   type PaidDatas = TreeMap[String, LandRegistryPricePaidDataItem]
 
   val paidDatas = ppds.map(t => t.transactionUniqueIdentifier -> t)(collection.breakOut) : PaidDatas
-  
+
   def merge(ppds: List[LandRegistryPricePaidDataItem]): LandRegistryPricePaidData = {
     new LandRegistryPricePaidData(ppds)
   }
--- src/test/scala/LandRegistryPricePaidDataSpec.scala
+++ src/test/scala/LandRegistryPricePaidDataSpec.scala
@@ -2,12 +2,16 @@ import org.scalatest._
 import org.scalatest.matchers.ShouldMatchers
 
 class LandRegistryPricePaidDataSpec extends FlatSpec with ShouldMatchers {
+  "paidDatas of empty constructor" should "have zero elements" in {
+    (new LandRegistryPricePaidData()).paidDatas.isEmpty should be === true
+  }
+
   "merge with an empty list" should "have zero elements" in {
-    (new LandRegistryPricePaidData(List())).merge(List()).paidDatas.isEmpty should be === true
+    (new LandRegistryPricePaidData()).merge(List()).paidDatas.isEmpty should be === true
   }
 
   "merge a list of one element" should "return a no empty map" in {
     val paidDataItem = new LandRegistryPricePaidDataItem()
-    new LandRegistryPricePaidData(List()).merge(List(paidDataItem)).paidDatas.isEmpty should be === false
+    new LandRegistryPricePaidData().merge(List(paidDataItem)).paidDatas.isEmpty should be === false
   }
 }
