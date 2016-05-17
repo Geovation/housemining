e511b03b609986469284a972a632e0a625b36095
Sebastian Ovide <Sebastian.Ovide@gmail.com>
2016-05-17 22:39:58 +0100

    added all csv fields

--- src/main/scala/LandRegistryPricePaidDataItem.scala
+++ src/main/scala/LandRegistryPricePaidDataItem.scala
@@ -2,10 +2,23 @@
 see https://www.gov.uk/government/statistical-data-sets/price-paid-data-downloads
 https://www.gov.uk/guidance/about-the-price-paid-data
 */
-class LandRegistryPricePaidDataItem(val transactionUniqueIdentifier: String, val price: Int, val recordStatus: Char = 'A') {
-  //var dateOfTransfer: java.util.Date = _
-  //var postcode: String = _
-  // more from https://www.gov.uk/guidance/about-the-price-paid-data
+import java.util.Date
   
-  
+class LandRegistryPricePaidDataItem(
+    val transactionUniqueIdentifier: String, 
+    val price: Int, 
+    val dateOfTransfer: Date,
+    val postcode: String,
+    val propertyType: Char,
+    val oldNew: Char,
+    val duration: Char,
+    val PAON: String,
+    val SAON: String,
+    val street: String,
+    val locality: String,
+    val townCity: String,
+    val District: String,
+    val County: String,
+    val PPDCategoryType: Char,
+    val recordStatus: Char) { 
 }
--- src/test/scala/LandRegistryPricePaidDataSpec.scala
+++ src/test/scala/LandRegistryPricePaidDataSpec.scala
@@ -1,13 +1,21 @@
 import org.scalatest._
 import org.scalatest.matchers.ShouldMatchers
 
+import java.util.Date
+
+
 class LandRegistryPricePaidDataSpec extends FlatSpec with ShouldMatchers {
+  
+  def createItem(id : String, price : Int, status : Char = 'C'): LandRegistryPricePaidDataItem =
+    new LandRegistryPricePaidDataItem(id, price, new Date(), "", ' ', ' ', ' ', "", "","","","","","", ' ', status)
+  
+ 
   "paidDatas of empty constructor" should "have zero elements" in {
     (new LandRegistryPricePaidData()).paidDatas.isEmpty should be === true
   }
 
   "paidDatas of constructor with one element" should "have elements" in {
-    val paidDataItem = new LandRegistryPricePaidDataItem("1",1)
+    val paidDataItem = createItem("1",1)
     (new LandRegistryPricePaidData(List(paidDataItem))).paidDatas.isEmpty shouldBe false
   }
 
@@ -16,20 +24,20 @@ class LandRegistryPricePaidDataSpec extends FlatSpec with ShouldMatchers {
   }
 
   "merge a list of one element" should "return a no empty map" in {
-    val paidDataItem = new LandRegistryPricePaidDataItem("1",1)
+    val paidDataItem = createItem("1",1)
     new LandRegistryPricePaidData().merge(List(paidDataItem)).paidDatas.isEmpty shouldBe false
   }
 
   "merge a list of one element with the same" should "return the same list" in {
-    val paidDataItem = new LandRegistryPricePaidDataItem("1",1)
+    val paidDataItem = createItem("1",1)
     val paidData = new LandRegistryPricePaidData(List(paidDataItem))
       
     paidData.merge(List(paidDataItem)).paidDatas should equal (paidData.paidDatas)
   }
   
   "merge a list of one element with a different list" should "return the two list added" in {
-    val list1 = List(new LandRegistryPricePaidDataItem("1",1))
-    val list2 = List(new LandRegistryPricePaidDataItem("2",2))
+    val list1 = List(createItem("1",1))
+    val list2 = List(createItem("2",2))
     val listAdd = list1:::list2
 
     val paidData1 = new LandRegistryPricePaidData(list1)
@@ -40,10 +48,10 @@ class LandRegistryPricePaidDataSpec extends FlatSpec with ShouldMatchers {
   }
   
   "merge" should "update existing values" in {
-    val el1 = new LandRegistryPricePaidDataItem("1",1)
-    val el2 = new LandRegistryPricePaidDataItem("2",2)
-    val el3 = new LandRegistryPricePaidDataItem("3",2)
-    val el1new = new LandRegistryPricePaidDataItem("1",10)
+    val el1 = createItem("1",1)
+    val el2 = createItem("2",2)
+    val el3 = createItem("3",2)
+    val el1new = createItem("1",10)
 
     val list1 = List(el1, el2)
     val list2 = List(el3, el1new)
@@ -57,10 +65,10 @@ class LandRegistryPricePaidDataSpec extends FlatSpec with ShouldMatchers {
   }
   
   "merge" should "delete a value" in {
-    val el1 = new LandRegistryPricePaidDataItem("1",1)
-    val el2 = new LandRegistryPricePaidDataItem("2",2)
-    val el3 = new LandRegistryPricePaidDataItem("3",2)
-    val el1new = new LandRegistryPricePaidDataItem("1",10, 'D')
+    val el1 = createItem("1",1)
+    val el2 = createItem("2",2)
+    val el3 = createItem("3",2)
+    val el1new = createItem("1",10, 'D')
 
     val list1 = List(el1, el2)
     val list2 = List(el3, el1new)
@@ -74,13 +82,13 @@ class LandRegistryPricePaidDataSpec extends FlatSpec with ShouldMatchers {
   }
 
   "merge" should "return the right values" in {
-    val el11 = new LandRegistryPricePaidDataItem("1",11)
-    val el12 = new LandRegistryPricePaidDataItem("2",12)
-    val el13 = new LandRegistryPricePaidDataItem("3",13)
+    val el11 = createItem("1",11)
+    val el12 = createItem("2",12)
+    val el13 = createItem("3",13)
 
-    val el21 = new LandRegistryPricePaidDataItem("1",21)
-    val el22 = new LandRegistryPricePaidDataItem("2",22, 'D')
-    val el23 = new LandRegistryPricePaidDataItem("4",24)
+    val el21 = createItem("1",21)
+    val el22 = createItem("2",22, 'D')
+    val el23 = createItem("4",24)
 
     
     val list1 = List(el11, el12, el13)
@@ -90,9 +98,6 @@ class LandRegistryPricePaidDataSpec extends FlatSpec with ShouldMatchers {
     val paidData1 = new LandRegistryPricePaidData(list1)
     val paidData2 = paidData1.merge(list2)
     val paidDataExpected = new LandRegistryPricePaidData(listExpected)
-    
-
-//    info(listAdd + "")
     
     paidData2.paidDatas should equal (paidDataExpected.paidDatas) 
   }  
