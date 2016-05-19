7193162c8d449d01e37bb08c39e4c2fdf79c97f2
Sebastian Ovide <Sebastian.Ovide@gmail.com>
2016-05-19 22:56:02 +0100

    another test merging files

--- src/test/scala/LandRegistryPricePaidDataMergerSpec.scala
+++ src/test/scala/LandRegistryPricePaidDataMergerSpec.scala
@@ -13,9 +13,10 @@ class LandRegistryPricePaidDataMergerSpec extends FlatSpec with ShouldMatchers {
       
       // create temp dir 
       val destDir = System.getProperty("java.io.tmpdir")
+
+      // files
       val destCsvFile = new File(destDir, "dest.csv" )
-      val dataCsv = getClass.getResource("/data.csv")
-      val dataCsvFile = new File(dataCsv.getFile)
+      val dataCsvFile = new File(getClass.getResource("/data.csv").getFile)
         
       val writer = new PrintWriter(destCsvFile)
       writer.close()
@@ -34,4 +35,36 @@ class LandRegistryPricePaidDataMergerSpec extends FlatSpec with ShouldMatchers {
 
       mapData should equal (mapDest)
     }
-}
+
+    "csv files" should "be merged" in {
+      // create temp dir
+      val destDir = System.getProperty("java.io.tmpdir")
+
+      // files
+      val destCsvFile = new File(destDir, "dest.csv" )
+      val dataCsvFile = new File(getClass.getResource("/data.csv").getFile)
+      val data2CsvFile = new File(getClass.getResource("/data-2.csv").getFile)
+      val dataMergedCsvFile = new File(getClass.getResource("/data-merged.csv").getFile)
+
+
+      val writer = new PrintWriter(destCsvFile)
+      writer.close()
+
+      // merge with data.csv
+      LandRegistryPricePaidDataMerger.mergeCsv(destCsvFile, dataCsvFile)
+
+      // merge with data-2.csv
+      LandRegistryPricePaidDataMerger.mergeCsv(destCsvFile, data2CsvFile)
+
+      // compare the two files line by line
+      val readerData = CSVReader.open(dataMergedCsvFile)
+      val readerDest = CSVReader.open(destCsvFile)
+
+      val mapData = readerData.all.map(l => l(0) -> l).toMap
+      val mapDest = readerDest.all.map(l => l(0) -> l).toMap
+      readerData.close
+      readerDest.close
+
+      mapData should equal (mapDest)
+    }
+  }
