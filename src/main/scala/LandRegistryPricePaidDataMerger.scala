f869bf530e32a9f8bd744b8f9d1a6ce6cf3c18f1
Sebastian Ovide <Sebastian.Ovide@gmail.com>
2016-05-27 17:53:08 +0100

    fix url

--- src/main/scala/LandRegistryPricePaidDataMerger.scala
+++ src/main/scala/LandRegistryPricePaidDataMerger.scala
@@ -10,21 +10,27 @@ import org.joda.time.DateTime
 object LandRegistryPricePaidDataMerger extends App {
 
   val CurrentCsv = "LandRegistryPricePaidData.csv"
-  val InputCsv = "Input.csv"
+  val MonthlyCsv = "Monthly.csv"
   val CompleteUrl = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-complete.csv"
-  val MonthlyUrl = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-monthly-update.csv"
+  val MonthlyUrl = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-monthly-update-new-version.csv"
 
   // Get the big one ? (if CurrentCsv is not there)
   if (shouldGetComplete(CurrentCsv)) download(CompleteUrl, CurrentCsv)
 
   // Get and merge a monthly one ? (based on CurrentCsv update time)
-  if (shouldGetMonthly(CurrentCsv)) download(MonthlyUrl, CurrentCsv)
+  if (shouldGetMonthly(MonthlyCsv)) download(MonthlyUrl, MonthlyCsv)
+
+  mergeCsv(new File(CurrentCsv), new File(MonthlyCsv))
 
   // Daemon ? and run it again (next month)
   // ..
   // TODO
 
-  def download(fromUrl: String, toFile: String): Unit = new URL(fromUrl) #> new File(toFile) !!
+  def download(fromUrl: String, toFile: String): Unit = {
+    println("Downloading " + toFile + " from " + fromUrl)
+    new URL(fromUrl) #> new File(toFile) !!;
+    println(toFile + " downloaded")
+  }
 
   /**
     * In order to get the complete file, currentCsv should not exist
@@ -48,18 +54,22 @@ object LandRegistryPricePaidDataMerger extends App {
   }
 
   def mergeCsv(currentCsv: File, inputCsv: File) {
-    
+    println("merging " + inputCsv + " into " + currentCsv)
+
     // read currentCsv
+    println("Reading " + currentCsv)
     val readerCurrentCsv = CSVReader.open(currentCsv)
     val currentList = readerCurrentCsv.all.map(line =>  LandRegistryPricePaidDataItem(line))
     readerCurrentCsv.close()
 
     // read inputCsv
+    println("Reading " + inputCsv)
     val readerInputCsv = CSVReader.open(inputCsv)
     val inputList = readerInputCsv.all.map(line =>  LandRegistryPricePaidDataItem(line))
     readerInputCsv.close()
 
     // merge
+    println("Merging")
     val merged = new LandRegistryPricePaidData(currentList).merge(inputList)
 
     // save to currentCsv
@@ -69,6 +79,7 @@ object LandRegistryPricePaidDataMerger extends App {
       writer.write(d._2.toString() + "\n")
     })
     writer.close()
-    
+
+    println(inputCsv + "merged into " + currentCsv)
   }
 }
